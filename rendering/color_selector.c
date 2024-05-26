a8d0426799748a87a4c4f7dc22122b5b72530f3e
Jesse Williams <jesse@negativespacesounds.com>
2024-05-26 16:17:01 +1000

    update: make color selector horizontally oriented

--- rendering/color_selector.c
+++ rendering/color_selector.c
@@ -1,6 +1,3 @@
-#ifndef COLOR_SELECTOR_H
-#define COLOR_SELECTOR_H
-
 #include "./color_selector.h"
 #include "../types/colors.h"
 #include <SDL2/SDL.h>
@@ -9,15 +6,13 @@ void renderColorSelectorGrid(SDL_Renderer *renderer) {
   SDL_Rect rect;
   rect.w = BOX_SIZE;
   rect.h = BOX_SIZE;
-  for (int i = 0; i < 4; ++i) {
+  for (int i = 0; i < 2; ++i) {
     for (int j = 0; j < 4; ++j) {
-      rect.x = i * BOX_SIZE;
-      rect.y = j * BOX_SIZE;
+      rect.x = j * BOX_SIZE;
+      rect.y = i * BOX_SIZE;
       SDL_SetRenderDrawColor(renderer, COLORS[i * 4 + j].r, COLORS[i * 4 + j].g,
                              COLORS[i * 4 + j].b, COLORS[i * 4 + j].a);
       SDL_RenderFillRect(renderer, &rect);
     }
   }
 }
-
-#endif
